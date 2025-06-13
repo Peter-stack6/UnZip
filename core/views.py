cf1c2e60a9b21b20b0db1e84cefb25fbc82b9ef3
Peter-stack6 <iseyemipiro@gmail.com>
2025-06-14 00:31:29 +0100

    Update views.py

--- core/views.py
+++ core/views.py
@@ -97,7 +97,7 @@ def UploadFile(request):
                         rarfile.UNRAR_TOOL = os.path.join('unrar', 'unrar.exe')
                         with rarfile.RarFile(file_bytes) as rar:
                             tree = True
-                            if not rf.needs_password():
+                            if not rar.needs_password():
                                 extracted_paths = [info.filename for info in rar.infolist() if not info.isdir()]
                             else:
                                 encrypted = True
