371fcdbb2658032cb33086cf3ea83f1fc706abf3
Peter-stack6 <iseyemipiro@gmail.com>
2025-06-20 16:29:40 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
+
+from sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path("", views.HomePage, name = 'home-page'),
@@ -8,5 +18,9 @@ urlpatterns = [
     path("single-file/", views.SingleFile, name = 'single-file'),
     path("tree/", views.Tree, name = 'tree'),
     path("download-single/", views.Download, name = 'download-single'),
-    path("download/", views.DownloadZip, name = 'download')
+    path("download/", views.DownloadZip, name = 'download'),
+
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
